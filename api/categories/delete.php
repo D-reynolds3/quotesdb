b5713b52af7a23a822cc56a3846679ba050de9da
D-reynolds3 <djreyn83@gmail.com>
2023-03-23 09:04:44 +0800

    delete change

--- api/categories/delete.php
+++ api/categories/delete.php
@@ -8,21 +8,17 @@
     include_once '../../config/Database.php';
     include_once '../../models/Category.php';
   
-    //create database object
+    
     $database = new Database();
     $db = $database->connect();
   
-    // create category object
     $cat = new Category($db);
 
-    //get data
     $data = json_decode(file_get_contents("php://input"));
 
-    // if missing data message and quit
     if (!isset($data->id)){
         echo(json_encode(array('message' => 'Missing Required Parameters')));
     }
-    // otherwise delete category
     else
     {
         $cat->id = $data->id;
